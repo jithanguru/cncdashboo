67cc4b62bbfe88e4b6096b9d77130b30aa3aa235
JITHU <137611376+jithanguru@users.noreply.github.com>
2026-02-12 20:47:48 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./Home.css"
+import { cncData,machineLive } from "./MachineData";
+
+
+
+export default function Home() {
+  const [selectedMachine, setSelectedMachine] = useState(cncData[0]);
+
+   const [selectedMachineLive, setSelectedMachineLive] = useState(machineLive[0]);
+
+  return (
+    <div className="container">
+      {/* Dashboard */}
+      <div className="dashboard">
+        <h2 className="dashboard-label">Dashboard</h2>
+        <div className="dashboard-table">
+        
+          <div>
+               <p><strong>Machine:</strong> <span>{selectedMachine.name}</span></p>
+               <p><strong>Status:</strong><span> {selectedMachine.status}</span></p>
+               <p><strong>Temperature:</strong> <span>{selectedMachine.temperature}</span></p>
+           </div>
+           <div>
+                <p><strong>Load:</strong> <span>{selectedMachine.load}</span></p>
+                <p><strong>Start time:</strong><span> {selectedMachine.startTime}</span></p>
+                <p><strong>End time :</strong><span> {selectedMachine.endTime}</span></p>
+           </div>
+         </div>
+
+
+
+
+      </div>
+
+      {/* CNC Machine Roadmap */}
+      <div className="cnc-container">
+        {cncData.map((machine) => (
+          <button
+            key={machine.id}
+            onClick={() => setSelectedMachine(machine)}
+            className="machine-button"
+          >
+            {machine.name}
+          </button>
+        ))}
+      </div>
+
+      {/*cnc machine live updates */}
+
+          <div className="live-updates-container">
+             <strong >live updates </strong>
+           
+
+            <div>
+             
+            </div>
+
+          </div>
+
+
+
+    </div>
+  );
+}
---
+++ src/MachineData.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+// Dummy CNC machine details
+
+ export const cncData = [
+  { id: 1, name: "CNC Machine 1", status: "Running", temperature: "72°C", load: "65%", startTime:"16:28:11", endTime:"--" },
+  { id: 2, name: "CNC Machine 2", status: "Idle", temperature: "60°C", load: "10%", startTime:"--", endTime:"17:20:08" },
+  { id: 3, name: "CNC Machine 3", status: "Maintenance", temperature: "--", load: "--" , startTime:"--", endTime:"--"},
+  { id: 4, name: "CNC Machine 4", status: "Running", temperature: "80°C", load: "78%" , startTime:"16:27:51", endTime:"--"},
+  { id: 5, name: "CNC Machine 5", status: "Error", temperature: "95°C", load: "90%" , startTime:"13:31:15", endTime:"14:17:23"},
+  { id: 6, name: "CNC Machine 6", status: "Idle", temperature: "55°C", load: "5%" , startTime:"13:28:17", endTime:"18:10:29"},
+  { id: 7, name: "CNC Machine 7", status: "Running", temperature: "70°C", load: "50%" , startTime:"13:28:17", endTime:"--"},
+  { id: 8, name: "CNC Machine 8", status: "Running", temperature: "89°C", load: "82%" , startTime:"12:07:45", endTime:"--"},
+
+];
+
+
+ 
+
+//  now time 19:30:00 
+
+export const machineLive = [
+  {id:1, name:"name", }
+];
+
